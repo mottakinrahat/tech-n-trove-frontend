7975331f4b3518db1b4beac78be57a9d7a8e9f32
SayedLabib <alfuad1007@gmail.com>
2024-05-21 00:24:48 +0600

    card section updated

--- src/components/UI/HomePage/TrendingProducts/TrendingProductCard.tsx
+++ src/components/UI/HomePage/TrendingProducts/TrendingProductCard.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Title from "../../Title/Title";
 import Image from "next/image";
+import Card from '@mui/material/Card';
 import { Box, Container, Stack, Typography } from "@mui/material";
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 // const ProductCard = [
 
@@ -15,40 +22,116 @@ import { Box, Container, Stack, Typography } from "@mui/material";
 
 const TrendingProductCard = () => {
     return (
-
-        <Container>
-        <div>
-            
-        <Box sx={{ marginTop: "100px" }}>
+       
+   <Container>
+        
+           
+      <div> 
+           
+          <Box sx={{ marginTop: "80px" }}>
+                  
           <Title
-            upperText="GO TO DAIly DEALS"
+            upperText="GO TO DAILY DEALS"
             heading="TRENDING PRODUCTS"
           ></Title>
            
-            <Box>
-               
-            sx={{
-                //   backgroundColor: "#ffffff",
-                //   boxShadow: "0 5px 12px lightgray",
-                //   width: "120px",
-                //   height: "120px",
-                //   display: "flex",
-                //   flexDirection: "column",
-                //   justifyContent: "center",
-                //   alignItems: "center",
-                 
-                }}
+          </Box>
 
 
+      {/* Card-1 Design  */}
 
-            </Box>
-        
+      <Box>
+      <Card sx={{ 
+        maxWidth: 320, 
+        borderRadius: 2, 
+        boxShadow: 'none', 
+        border: '1px solid #d3d3d3',
+        backgroundColor: 'white' 
+      }}>
+        <CardMedia
+          component="img"
+          alt="Web Cam"
+          height="80"
+          image="https://m.media-amazon.com/images/I/61-K2lXmHQL._AC_SL1500_.jpg"
+          padding = "8px"
+        />
+        <Box sx={{ padding: '8px 16px' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography variant="h6" component="div">
+            ৳ 3399
+            </Typography>
+            <Typography variant="body2" color="text.secondary" sx={{ textDecoration: 'line-through' }}>
+              4800
+            </Typography>
+          </Box>
+         
+          <Typography variant="body2" color="red" sx={{ marginTop: '4px' }}>
+            28% OFF
+          </Typography>
+
+          <Typography variant="body2" color="green" sx={{ marginTop: '4px' }}>
+            In stock
+          </Typography>
+
         </Box>
 
-            
-        </div>
-         
-         </Container>
+
+
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Web-Cam
+          </Typography>
+
+
+          <Typography variant="body2" color="text.secondary">
+            High-definition webcam with 1080p resolution for crisp video quality.
+          </Typography>
+
+
+        </CardContent>
+
+
+
+
+        <CardActions>
+        <Button 
+
+
+  size="small" 
+
+  startIcon={<ShoppingCartIcon />}
+>
+  Add To Cart
+       </Button>
+       
+       
+       <Button 
+            size="small" 
+
+          >
+           See details
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+       
+
+      
+
+      </div>
+
+
+
+
+
+
+
+   </Container>
+
+    
+
+
+
 
     );
 };
