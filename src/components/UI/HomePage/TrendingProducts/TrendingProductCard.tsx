3dc2df3da46b8123e017ce1b0103a8446865ae81
SayedLabib <alfuad1007@gmail.com>
2024-05-20 22:51:01 +0600

    updated

--- src/components/UI/HomePage/TrendingProducts/TrendingProductCard.tsx
+++ src/components/UI/HomePage/TrendingProducts/TrendingProductCard.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import Title from "../../Title/Title";
+import Image from "next/image";
+import { Box, Container, Stack, Typography } from "@mui/material";
+
+// const ProductCard = [
+
+// {
+//     productImage:"https://m.media-amazon.com/images/I/71W0+kFYV7L.jpg",
+//     title: "Web-Cam",
+// }
+
+// ]
+
 
 const TrendingProductCard = () => {
     return (
+
+        <Container>
         <div>
-            THis is MUI page...
+            
+        <Box sx={{ marginTop: "100px" }}>
+          <Title
+            upperText="GO TO DAIly DEALS"
+            heading="TRENDING PRODUCTS"
+          ></Title>
+           
+            <Box>
+               
+            sx={{
+                //   backgroundColor: "#ffffff",
+                //   boxShadow: "0 5px 12px lightgray",
+                //   width: "120px",
+                //   height: "120px",
+                //   display: "flex",
+                //   flexDirection: "column",
+                //   justifyContent: "center",
+                //   alignItems: "center",
+                 
+                }}
+
+
+
+            </Box>
+        
+        </Box>
+
+            
         </div>
+         
+         </Container>
+
     );
 };
 
